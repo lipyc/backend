a344796f9c5a186c8fe2d8797d03297eec990ed3
lipyc <62886820+lipyc@users.noreply.github.com>
2024-07-12 22:47:07 +0100

    Delete backend directory

--- backend/src/Routes/authRota.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../Controllers/authControlador');
-
-router.post('/login', auth.login);
-
-module.exports = router;
